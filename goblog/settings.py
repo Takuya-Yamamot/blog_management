0c11ae4267197020c42aa529ed204028791756f9
Takuya <takuya.miraidenshi@gmail.com>
2019-09-09 20:07:19 +0900

    fix: settings(before makemigrations)

--- goblog/settings.py
+++ goblog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "social_django",
     "mgblog",
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = "goblog.urls"
@@ -65,6 +67,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ]
         },
     }
@@ -100,7 +104,19 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
 
+AUTHENTICATION_BACKENDS = (
+ 'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+ 'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
+ 'django.contrib.auth.backends.ModelBackend',
+)
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '641114255425-cc71svia4rfbksb603k4dqk19ii81t25.apps.googleusercontent.com'  #Paste CLient Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'WZjxEb4IleamuBgZPgF7m6dI' #Paste Secret Key
+SOCIAL_AUTH_URL_NAMESPACE = 'mgblog:social'
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
--- mgblog/urls.py
+++ mgblog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from . import views
+from django.conf.urls import include
+from django.contrib.auth import views
+from .views import index, breif, plan_form
 
 app_name = "mgblog"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("breif/<int:clientId>", views.breif, name="breif"),
-    path("plan_form", views.plan_form, name="plan_form"),
+    path("", index, name="index"),
+    path("breif/<int:clientId>", breif, name="breif"),
+    path("plan_form", plan_form, name="plan_form"),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logged_out/', views.LogoutView.as_view(), name='logged_out'),
+    path('auth/', include('social_django.urls', namespace='social')),
 ]
